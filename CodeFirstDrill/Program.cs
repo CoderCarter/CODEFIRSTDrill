08026a23df99b5020e63189c94c12d40d0488933
Carter <codercarter@gmail.comgit config --global user.namce Cartergit config --global user.email codercarter@gmail.com>
2018-05-22 11:46:15 -0500

    first try after gitignore added

---
+++ CodeFirstDrill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                //create and save new blog
+                Console.WriteLine("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                //Display Blogs from db
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press the any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
